682586adabb187f3dfa3d07a1e6b326ec1c5faff
grifis <fujeba2abm@i.softbank.jp>
2022-08-06 19:34:32 +0900

    コンフリクトの解消

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -6,23 +6,14 @@ use App\Models\Comment;
 use App\Models\Portfolio;
 use App\Models\Tag;
 use Illuminate\Http\Request;
-<<<<<<< HEAD
 use Carbon\Carbon;
 use App\Models\User;
-
-
-=======
 use Illuminate\Support\Facades\Auth;
->>>>>>> 13db57885e7ca2f2e4c7672e185d951baa88eeb0
 
 class PortfolioController extends Controller
 {
     public function index(Portfolio $portfolio, Tag $tag, Request $request)
     {
-<<<<<<< HEAD
-        $items = $portfolio->get();
-        return view('index', compact('items'));
-=======
         $query = Portfolio::with('tag')->latest();
         $tag_id = $request->tags;
 
@@ -34,7 +25,6 @@ class PortfolioController extends Controller
         $tags = $tag->get();
         $items = $portfolio->latest()->paginate(10);
         return view('index', compact('items', 'tags'));
->>>>>>> 13db57885e7ca2f2e4c7672e185d951baa88eeb0
     }
 
     public function show(Portfolio $portfolio)
@@ -48,24 +38,21 @@ class PortfolioController extends Controller
         return view('create', compact('tags'));
     }
 
-    public function store(Portfolio $portfolio)
+    public function store(Portfolio $portfolio, Request $request)
     {
-<<<<<<< HEAD
-=======
         $tags = $request->input('tags');
         $input = $request->all();
         $portfolio = $portfolio->create($input);
 
         $portfolio->tags()->attach($tags);
->>>>>>> 13db57885e7ca2f2e4c7672e185d951baa88eeb0
         return redirect('/portfolio/' . $portfolio->id);
     }
-    
+
     public function create_comment($portfolio)
     {
         return view('create_comment', compact('portfolio'));
     }
-    
+
     public function store_comment(Comment $comment, Request $request)
     {
         $input = $request->all();
