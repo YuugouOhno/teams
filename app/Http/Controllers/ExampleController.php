362838728e6c24734a0bf02d6128cebc9a337bea
YuugouOhno <yuugou.ohno@gmail.com>
2022-08-06 12:23:23 +0900

    test

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -9,12 +9,6 @@ class ExampleController extends Controller
 {
     public function index()
     {
-        return view('welcome');
-    }
-
-    public function example()
-    {
-        $examples = Example::all();
-        return view('example', ['examples' => $examples]);
+        return view('index');
     }
 }
