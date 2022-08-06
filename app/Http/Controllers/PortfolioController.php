bd4df64765485907f6684a5cf6078fbfbc60c92d
YuugouOhno <yuugou.ohno@gmail.com>
2022-08-06 23:14:02 +0900

    ユーザーネームでログイン

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -14,16 +14,6 @@ class PortfolioController extends Controller
 {
     public function index(Portfolio $portfolio, Tag $tag, Request $request)
     {
-        $query = Portfolio::with('tag')->latest();
-        $tags_id = $request->tags;
-        
-        if($tag_id) {
-            foreach($tags_id as $tag_id){
-                $query->where('tag_id', $tag_id);
-            }
-        }
-        
-
         $tags = $tag->get();
         $items = $portfolio->latest()->paginate(10);
         return view('index', compact('items', 'tags'));
