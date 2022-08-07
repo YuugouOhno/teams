76caba0e2c7740b327d7c369f05bb9d5c3a03ccd
sho watanabe <showatanabe0226@gmail.com>
2022-08-07 16:56:59 +0900

    [update]adminのトップページのスタイルを修正

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,20 +7,32 @@
 <body>
     <form action="/admin/store_tag" method="POST">
         @csrf
-        <div>
-            <input type="text" name="name" placeholder="新規タグ名"/>
+        <div class="px-10 pt-5 ">
+            <h1 class="font-bold">タグの追加</h1>
         </div>
-        <div>
-            <input type="submit">
+        <div class="flex items-center px-10 py-5">
+
+            <div class="px-4">
+                <input class="rounded-md" type="text" name="name" placeholder="新規タグ名"/>
+            </div>
+            <div>
+                <button class="bg-indigo-400 px-5 py-2 rounded-md hover:bg-indigo-500 duration-100" type="submit">送信</button>
+            </div>
+
         </div>
     </form>
-    <h1>認証待ちポートフォリオ一覧ページ</h1>
-    @foreach($drafts as $draft)
-        <div class="border-solid border-2 border-sky-500 mb-4">
-            <h2>タイトル：<a href="/admin/{{ $draft->id }}">{{ $draft->title }}</a></h2>
-            <h2>開発者：{{ $draft->developer }}</h2>
-            <h2>担当メンター：{{ $draft->mentor }}</h2>
-        </div>
-    @endforeach
+
+    <div class="px-10 pt-5 font-bold">
+        <h1>認証待ちポートフォリオ一覧ページ</h1>
+    </div>
+    <div class="px-14 pt-5">
+        @foreach($drafts as $draft)
+            <div class="border-solid border-2 border-sky-500 mb-4">
+                <h2 class="m-2">タイトル：<a class="font-bold text-indigo-400 hover:text-pink-600" href="/admin/{{ $draft->id }}">{{ $draft->title }}</a></h2>
+                <h2 class="m-2">開発者：{{ $draft->developer }}</h2>
+                <h2 class="m-2">担当メンター：{{ $draft->mentor }}</h2>
+            </div>
+        @endforeach
+    </div>
 </body>
 </x-app-layout>
