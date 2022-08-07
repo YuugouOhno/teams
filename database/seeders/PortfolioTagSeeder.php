e128ecd28799d22ef9b07405e287ef817eb1b8d6
RyutaNoguchi <ryuta.noguchi.levtechcollege@gmail.com>
2022-08-07 15:57:34 +0900

    タグの選定

--- database/seeders/PortfolioTagSeeder.php
+++ database/seeders/PortfolioTagSeeder.php
@@ -17,55 +17,91 @@ class PortfolioTagSeeder extends Seeder
     {
         DB::table('portfolio_tag')->insert([
             "portfolio_id" => 1,
-            "tag_id" => 1,
-            "created_at" => date("2022-08-06 17:53:21"),
-            "updated_at" => date("2022-08-06 17:53:21"),
-        ]);
-        DB::table('portfolio_tag')->insert([
-            "portfolio_id" => 2,
-            "tag_id" => 2,
+            "tag_id" => 9,
             "created_at" => date("2022-08-06 17:53:21"),
             "updated_at" => date("2022-08-06 17:53:21"),
         ]);
         DB::table('portfolio_tag')->insert([
             "portfolio_id" => 3,
+            "tag_id" => 12,
+            "created_at" => date("2022-08-06 17:53:21"),
+            "updated_at" => date("2022-08-06 17:53:21"),
+        ]);
+        DB::table('portfolio_tag')->insert([
+            "portfolio_id" => 5,
+            "tag_id" => 1,
+            "created_at" => date("2022-08-06 17:53:21"),
+            "updated_at" => date("2022-08-06 17:53:21"),
+        ]);
+        DB::table('portfolio_tag')->insert([
+            "portfolio_id" => 5,
+            "tag_id" => 3,
+            "created_at" => date("2022-08-06 17:53:21"),
+            "updated_at" => date("2022-08-06 17:53:21"),
+        ]);
+        DB::table('portfolio_tag')->insert([
+            "portfolio_id" => 6,
+            "tag_id" => 17,
+            "created_at" => date("2022-08-06 17:53:21"),
+            "updated_at" => date("2022-08-06 17:53:21"),
+        ]);
+        DB::table('portfolio_tag')->insert([
+            "portfolio_id" => 6,
+            "tag_id" => 4,
+            "created_at" => date("2022-08-06 17:53:21"),
+            "updated_at" => date("2022-08-06 17:53:21"),
+        ]);
+        DB::table('portfolio_tag')->insert([
+            "portfolio_id" => 4,
+            "tag_id" => 8,
+            "created_at" => date("2022-08-06 17:53:21"),
+            "updated_at" => date("2022-08-06 17:53:21"),
+        ]);
+        DB::table('portfolio_tag')->insert([
+            "portfolio_id" => 4,
+            "tag_id" => 2,
+            "created_at" => date("2022-08-06 17:53:21"),
+            "updated_at" => date("2022-08-06 17:53:21"),
+        ]);
+        DB::table('portfolio_tag')->insert([
+            "portfolio_id" => 4,
+            "tag_id" => 19,
+            "created_at" => date("2022-08-06 17:53:21"),
+            "updated_at" => date("2022-08-06 17:53:21"),
+        ]);
+        DB::table('portfolio_tag')->insert([
+            "portfolio_id" => 4,
+            "tag_id" => 20,
+            "created_at" => date("2022-08-06 17:53:21"),
+            "updated_at" => date("2022-08-06 17:53:21"),
+        ]);
+        DB::table('portfolio_tag')->insert([
+            "portfolio_id" => 4,
+            "tag_id" => 21,
+            "created_at" => date("2022-08-06 17:53:21"),
+            "updated_at" => date("2022-08-06 17:53:21"),
+        ]);
+        DB::table('portfolio_tag')->insert([
+            "portfolio_id" => 4,
+            "tag_id" => 10,
+            "created_at" => date("2022-08-06 17:53:21"),
+            "updated_at" => date("2022-08-06 17:53:21"),
+        ]);
+        DB::table('portfolio_tag')->insert([
+            "portfolio_id" => 1,
             "tag_id" => 3,
             "created_at" => date("2022-08-06 17:53:21"),
             "updated_at" => date("2022-08-06 17:53:21"),
         ]);
         DB::table('portfolio_tag')->insert([
             "portfolio_id" => 4,
-            "tag_id" => 4,
-            "created_at" => date("2022-08-06 17:53:21"),
-            "updated_at" => date("2022-08-06 17:53:21"),
-        ]);
-        DB::table('portfolio_tag')->insert([
-            "portfolio_id" => 5,
-            "tag_id" => 5,
+            "tag_id" => 24,
             "created_at" => date("2022-08-06 17:53:21"),
             "updated_at" => date("2022-08-06 17:53:21"),
         ]);
         DB::table('portfolio_tag')->insert([
             "portfolio_id" => 6,
-            "tag_id" => 6,
-            "created_at" => date("2022-08-06 17:53:21"),
-            "updated_at" => date("2022-08-06 17:53:21"),
-        ]);
-        DB::table('portfolio_tag')->insert([
-            "portfolio_id" => 1,
-            "tag_id" => 7,
-            "created_at" => date("2022-08-06 17:53:21"),
-            "updated_at" => date("2022-08-06 17:53:21"),
-        ]);
-        DB::table('portfolio_tag')->insert([
-            "portfolio_id" => 3,
-            "tag_id" => 8,
-            "created_at" => date("2022-08-06 17:53:21"),
-            "updated_at" => date("2022-08-06 17:53:21"),
-        ]);
-        DB::table('portfolio_tag')->insert([
-            "portfolio_id" => 5,
-            "tag_id" => 9,
+            "tag_id" => 3,
             "created_at" => date("2022-08-06 17:53:21"),
             "updated_at" => date("2022-08-06 17:53:21"),
         ]);
